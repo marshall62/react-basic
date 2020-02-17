45a44c952a65e6f5acfa59ca15b8af021f5af84d
marshall62 <marshall62@gmail.com>
2020-02-17 10:03:00 -0500

    changed to full URL of backend

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,6 +2,6 @@ const proxy = require("http-proxy-middleware");
 
 module.exports = function(app) {
   app.use(
-    proxy(["/api"], { target: "http://localhost:8000", changeOrigin: true })
+    proxy(["/api"], { target: "http://flask-pairup.herokuapp.com", changeOrigin: true })
   );
 };
