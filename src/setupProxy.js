329fd4da9cde8aec5294b6ab85499749a010a4bd
marshall62 <marshall62@gmail.com>
2020-02-17 10:25:37 -0500

    proxy URL taken from .env file

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,9 +1,7 @@
 const proxy = require("http-proxy-middleware");
 
 module.exports = function(app) {
-  let host_url = process.env.REACT_APP_BACKEND_URI;
-  console.log("Host for backend is ", host_url);
   app.use(
-    proxy(["/api"], { target: host_url, changeOrigin: true })
+    proxy(["/api"], { target: "https://flask-pairup.herokuapp.com", changeOrigin: true })
   );
 };
